afca6a37c494f577637b132a4b8bd7dc9dc1e09d
sarujan <sarujansivanantharaja@gmail.com>
2022-05-28 18:32:09 +0530

    init sarujan branch

---
+++ src/Components/Hi.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Typography} from "@mui/material"
+
+const Hi = () => {
+  return (
+    <Typography variant="h1" color="initial"> Sarujan </Typography>
+  )
+}
+
+export default Hi
--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,9 +1,10 @@
 import { Paper, Typography } from "@mui/material";
-
+import Hi from "../Components/Hi" 
 function App() {
   return (
    <Paper elevation={3} bgvolor="background.asd">
      <Typography variant="h1" color="initial">YAZHL CITY</Typography>
+     <Hi/>
    </Paper>
   );
 }
