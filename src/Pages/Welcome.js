32575f1b200195abc1540b9b1bca0a830a448070
Rizni <mnriznimohamed@gmail.com>
2022-06-09 14:46:02 +0530

    footer

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,11 +1,15 @@
-import { Paper, Typography } from '@mui/material'
+import { Box, Paper, Typography } from '@mui/material'
 import React from 'react'
+import Footer from '../Components/Footer'
 
 const Welcome = () => {
   return (
-      <Paper elevation={3}>
-          <Typography variant="h1" color="background.secondarybg">YAZHL CITY</Typography>
-      </Paper>
+    <>
+      <Box display="flex" justifyContent="center" alignItems="center" height="calc(70vh - 64px)">
+        <Typography variant="h1" color="background.secondarybg">YAZHL CITY</Typography>
+      </Box>
+      <Footer />
+    </>
   )
 }
 
